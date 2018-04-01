2e474c5d50ad90fbe27890761374d7a9dbd0939e
Tara Nagar <tara.nagar@duke.edu>
2018-04-01 17:26:40 -0400

    Initial commit.

---
+++ review_sample_parse.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import json
+from pprint import pprint
+
+data = []
+# open review.json file to read from
+with open('../yelp-data/review.json', 'r') as rf:
+    # read in first 100 lines
+    for i in range(100):
+        line = rf.next().strip()
+        data.append(json.loads(line))
+rf.close()
+
+# open review_sample.json to write to
+with open('data_sample/review_sample.json', 'w') as wf:
+    # save 100 review lines into sample json
+    json.dump(data, wf)
+wf.close()
+
+pprint(data)
+del data[:]
+pprint(data)
