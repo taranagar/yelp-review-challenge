7c9d1d4ac1eefe5ac91a9684ba7631cd7044fd79
jiecai1997 <jiecai1997@gmail.com>
2018-04-17 23:12:01 -0400

    updated data parse criteria: places between 10-20 reviews

--- scripts/parse/business_parse.py
+++ scripts/parse/business_parse.py
@@ -5,20 +5,31 @@ from pprint import pprint
 
 data1 = []
 
-# open review.json file to read from
+# open business.json file to read from
 with open('../../../yelp-data/business.json', 'r') as rf:
-    # read in first 100 lines
     for line in rf:
         line_json = json.loads(line)
-        if 'GoodForMeal' in line_json['attributes'] and line_json['is_open'] == 1 and line_json['city'] == 'Las Vegas' and line_json['review_count'] >=20 and line_json['review_count'] <=50 and "Restaurants" in line_json['categories'] and "Fast Food" not in line_json['categories']:
+        # SELECT BUSINESSES THAT FOLLOW FOLLOWING CRITERIA:
+        ## good for meal - dinner = True
+        ## isopen = True
+        ## city = Las Vegas
+        ## 10 <= review_count <= 20
+        ## categories != Fast Food
+        if 'GoodForMeal' in line_json['attributes'] and line_json['is_open'] == 1 and line_json['city'] == 'Las Vegas' and line_json['review_count'] >=10 and line_json['review_count'] <=20 and "Restaurants" in line_json['categories'] and "Fast Food" not in line_json['categories']:
             if line_json['attributes']['GoodForMeal']['dinner'] == True:
                 data1.append(json.loads(line))
 rf.close()
 
-print "# Matching:", len(data1)
+# of businesses that match above criteria
+print "# Businesses:", len(data1)
 
+# write ALL business information into businesses.json
 with open('../../data/business/businesses.json', 'w') as wf1:
     json.dump(data1, wf1)
 wf1.close()
 
-del data1[:]
+# write business_id information to business_id.json to match with reviews
+with open('../../data/business_ids.json', 'w') as wf2:
+    data2 = [entry['business_id'] for entry in data1]
+    json.dump(data2, wf2)
+wf2.close()
---
+++ scripts/parse/review_parse_new.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import json
+from pprint import pprint
+
+data = []
+ids = []
+
+# open review.json file to read from
+with open('../../data/business_ids.json', 'r') as dataf:
+    for line in dataf:
+        ids.append(json.loads(line))
+ids = ids[0]
+dataf.close()
+
+# Number of Business/Business IDs:
+print "# Business IDs:", len(ids)
+
+# open review.json file to read from
+with open('../../../yelp-data/review.json', 'r') as rf:
+    for line in rf:
+        line_json = json.loads(line)
+        # if review matches with one of our business_ids
+        if str(line_json['business_id']) in ids:
+            data.append(line_json)
+rf.close()
+
+# Number of Reviews:
+print "# Reviews:", len(data)
+
+# open reviews.json file to write to
+with open('../../data/review/reviews.json', 'w') as wf:
+    json.dump(data, wf)
+wf.close()
