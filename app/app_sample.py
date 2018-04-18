e82b3daacd25c70c77f166df4eee904d52b4f57a
jiecai1997 <jiecai1997@gmail.com>
2018-04-17 23:10:58 -0400

    implemented business and review retreival

--- app/app_sample.py
+++ app/app_sample.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
-from data import Businesses
+from data import Businesses, Reviews
 app = Flask(__name__)
 
 Businesses = Businesses()
+Reviews = Reviews()
 
 @app.route("/")
 def home_index():
@@ -17,6 +18,7 @@ def accuracy_index():
     return render_template('accuracy.html')
 
 @app.route('/biz/<biz_id>')
-def profile(biz_id):
-    business = [s for i, s in enumerate(Businesses) if biz_id in s["business_id"]]
-    return render_template('business.html', business = business[0])
+def biz_index(biz_id):
+    business = [b for i, b in enumerate(Businesses) if biz_id in b["business_id"]]
+    reviews = [r for j, r in enumerate(Reviews) if biz_id in r["business_id"]]
+    return render_template('business.html', business = business[0], reviews = reviews)
